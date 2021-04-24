87cad6534018c5185721979d8089998f6c6e821e
wizwingwing <61519634+wizwingwing@users.noreply.github.com>
2021-04-24 13:56:45 +0700

    comit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./Contianer/Navbar"
+import Card from "./Contianer/Card"
+import "./Css/index" 
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Css/index.js
@@ -0,0 +1,3 @@
+import "./index.css"
+import "./navbar.css"
+import "./card.css"
